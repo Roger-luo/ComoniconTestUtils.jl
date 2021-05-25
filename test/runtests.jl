e2bde89b47dae35ec581fe6b084e4c792b33a404
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-25 05:28:51 -0400

    test random command generation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using ComoniconTypes
 using ComoniconTestUtils
 using Test
 
@@ -7,4 +8,27 @@ using Test
     @test_kwargs [:option_a=>1, :option_b=>2]    
 end
 
-rand_command()
+
+find_leaf(cmd::Entry, inputs, current=1) = find_leaf(cmd.root, inputs, current)
+find_leaf(cmd::LeafCommand, inputs, current=1) = cmd
+
+function find_leaf(cmd::NodeCommand, inputs, current=1)
+    @test haskey(cmd.subcmds, inputs[current])
+    return find_leaf(cmd.subcmds[inputs[current]], inputs, current+1)
+end
+
+@testset "random_command + random_input" begin
+    for _ in 1:10
+        cmd = rand_command()
+        inputs = rand_input(cmd)
+        leaf = find_leaf(cmd, inputs)
+
+        for each in inputs
+            if startswith(each, "--")
+                name = first(split(each, '='))
+                name = lstrip(name, '-')
+                @test haskey(leaf.options, name) || haskey(leaf.flags, name)
+            end
+        end
+    end
+end
